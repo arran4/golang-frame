029ef23150b587db934452b41f1d8bb9529233d9
Arran Ubels <arran4@gmail.com>
2022-06-09 21:15:56 +1000

    Minor refactor and additional documentation

--- frame.go
+++ frame.go
@@ -1,6 +1,7 @@
 package frame
 
 import (
+	"github.com/arran4/golang-frame/draw"
 	"image"
 	"image/color"
 	"math"
@@ -92,23 +93,10 @@ func (s5 *Section5) Apply(p image.Point, op image.Point, f *Frame, midStartX int
 	}
 	destc := f.Base.At(p.X+f.Base.Bounds().Min.X, p.Y+f.Base.Bounds().Min.Y)
 	if s5.AlphaMode == nil {
-		return Over(destc, a1, r1, g1, b1)
+		return draw.Over(destc, a1, r1, g1, b1)
 	} else {
 		return s5.AlphaMode(destc, a1, r1, g1, b1)
 	}
-}
-
-// Over is the alpha mode applied by draw.Draw() Probably inaccurate.
-func Over(destc color.Color, a1 uint32, r1 uint32, g1 uint32, b1 uint32) color.Color {
-	destr, destg, destb, desta := destc.RGBA()
-	reva := (math.MaxUint16 - a1) * 0x101
-	c := color.RGBA{
-		R: uint8((uint32(destr)*reva/math.MaxUint16 + r1*a1) >> 8),
-		G: uint8((uint32(destg)*reva/math.MaxUint16 + g1*a1) >> 8),
-		B: uint8((uint32(destb)*reva/math.MaxUint16 + b1*a1) >> 8),
-		A: uint8((uint32(desta)*reva/math.MaxUint16 + a1) >> 8),
-	}
-	return &c
 }
 
 // MiddleRect calculates the total space in the resulting image that section 5 consumes
